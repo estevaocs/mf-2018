7c2728dd3322e942bb24653561b5ce240c15c705
João Pedro Vieira <jp-a.v@hotmail.com>
2018-09-10 19:15:38 -0300

    criacao da Classe Aplicacao

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,11 @@
+package br.com.caelum.contas;
+
+import java.time.LocalDate;
+
+class Aplicacao {
+
+	public static void main(String[] args) {
+		System.out.printl(LocalDate.now().getDayOfWeek()).
+	}
+}
+
